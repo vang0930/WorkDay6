54f4104d09102b4bd1aa863ab6a434682a4247a3
idsc3101 <idsc3101@gmail.com>
2014-10-06 12:49:31 -0500

    Created the project skeletons

---
+++ IntersectionProblem/IntersectionProblem/Form1.Designer.vb
@@ -0,0 +1,162 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtA = New System.Windows.Forms.TextBox()
+        Me.txtB = New System.Windows.Forms.TextBox()
+        Me.txtC = New System.Windows.Forms.TextBox()
+        Me.txtD = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.btnCompute = New System.Windows.Forms.Button()
+        Me.txtResult = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtA
+        '
+        Me.txtA.Location = New System.Drawing.Point(82, 29)
+        Me.txtA.Name = "txtA"
+        Me.txtA.Size = New System.Drawing.Size(100, 20)
+        Me.txtA.TabIndex = 0
+        '
+        'txtB
+        '
+        Me.txtB.Location = New System.Drawing.Point(231, 29)
+        Me.txtB.Name = "txtB"
+        Me.txtB.Size = New System.Drawing.Size(100, 20)
+        Me.txtB.TabIndex = 1
+        '
+        'txtC
+        '
+        Me.txtC.Location = New System.Drawing.Point(82, 105)
+        Me.txtC.Name = "txtC"
+        Me.txtC.Size = New System.Drawing.Size(100, 20)
+        Me.txtC.TabIndex = 2
+        '
+        'txtD
+        '
+        Me.txtD.Location = New System.Drawing.Point(231, 105)
+        Me.txtD.Name = "txtD"
+        Me.txtD.Size = New System.Drawing.Size(100, 20)
+        Me.txtD.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(56, 32)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(20, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "A="
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(205, 32)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(20, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "B="
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(56, 108)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(20, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "C="
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(204, 108)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(21, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "D="
+        '
+        'btnCompute
+        '
+        Me.btnCompute.Location = New System.Drawing.Point(161, 158)
+        Me.btnCompute.Name = "btnCompute"
+        Me.btnCompute.Size = New System.Drawing.Size(89, 36)
+        Me.btnCompute.TabIndex = 8
+        Me.btnCompute.Text = "Compute Intersection"
+        Me.btnCompute.UseVisualStyleBackColor = True
+        '
+        'txtResult
+        '
+        Me.txtResult.Location = New System.Drawing.Point(82, 244)
+        Me.txtResult.Name = "txtResult"
+        Me.txtResult.Size = New System.Drawing.Size(249, 20)
+        Me.txtResult.TabIndex = 9
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(37, 247)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(37, 13)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "Result"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(387, 336)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtResult)
+        Me.Controls.Add(Me.btnCompute)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtD)
+        Me.Controls.Add(Me.txtC)
+        Me.Controls.Add(Me.txtB)
+        Me.Controls.Add(Me.txtA)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "Form1"
+        Me.Text = "Intersection Problem"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtA As System.Windows.Forms.TextBox
+    Friend WithEvents txtB As System.Windows.Forms.TextBox
+    Friend WithEvents txtC As System.Windows.Forms.TextBox
+    Friend WithEvents txtD As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents btnCompute As System.Windows.Forms.Button
+    Friend WithEvents txtResult As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+
+End Class
---
+++ IntersectionProblem/IntersectionProblem/Form1.vb
@@ -0,0 +1,33 @@
+﻿Option Strict On
+
+Public Class Form1
+
+    Private Sub btnCompute_Click(sender As Object, e As EventArgs) Handles btnCompute.Click
+        Dim Ast, Bst, Cst, Dst As String
+        Dim A, B, C, D As Double
+
+        ' Read the input from the user
+        Ast = txtA.Text
+        Bst = txtB.Text
+        Cst = txtC.Text
+        Dst = txtD.Text
+
+        ' All input must be numbers
+        If Not (IsNumeric(Ast) And IsNumeric(Bst) And IsNumeric(Cst) _
+                And IsNumeric(Dst)) Then
+            MessageBox.Show("Please enter valid numbers", "Error!")
+            Return
+        End If
+
+        ' Convert to double
+        A = CDbl(Ast)
+        B = CDbl(Bst)
+        C = CDbl(Cst)
+        D = CDbl(Dst)
+
+        ' Please write your code here...
+
+        txtResult.Text = "Not implemented yet"
+    End Sub
+
+End Class
